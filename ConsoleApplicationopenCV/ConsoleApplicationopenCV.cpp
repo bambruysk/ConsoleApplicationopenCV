d3a689943a6b47a2535425ad54a291ffde6de342
ралдугина анна <anna.raldugina98@gmail.com>
2020-07-29 15:26:26 +0300

    cv::Mat

--- ConsoleApplicationopenCV/ConsoleApplicationopenCV.cpp
+++ ConsoleApplicationopenCV/ConsoleApplicationopenCV.cpp
@@ -1,15 +1,18 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
-
+    //#include <opencv2/imgproc/imgproc.hpp>
+    //#include "opencv2/imgcodecs.hpp"
 #include <iostream>
 using namespace cv;
 using namespace std;
+
 int main(int argc, char** argv)
 {
     Mat image;
-    image = imread("biggrin.bmp", IMREAD_COLOR); // Read the file
-    //cv::namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
-    //imshow("Display window", image); // Show our image inside it.
+    image = imread("biggrin.bmp", IMREAD_REDUCED_COLOR_2); // Read the file
+    cv::namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
+        //cv::Mat(image, dst, CV_GAUSSIAN, 3, 3);
+    imshow("Display window", image); // Show our image inside it.
     waitKey(0); // Wait for a keystroke in the window
     return 0;
 }
